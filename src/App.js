4c1ae390c1cf0bb1c5ff00fd37ab80a28845b160
Ola <aleksandra.kurdel@gmail.com>
2024-01-25 19:17:01 +0100

    code refactoring and added a decrement button

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 
   state = {
     counters: [
-      { id: 1, value: 4 },
+      { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
@@ -20,6 +20,14 @@ class App extends Component {
     const index = counters.indexOf(counter);
     counters[index] = { ...counter };
     counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
     this.setState({ counters });
   };
 
@@ -42,7 +50,7 @@ class App extends Component {
 
         <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
         <main className='container'>
-          <Counters counters={this.state.counters} onIncrement={this.handleIncrement} onDelete={this.handleDelete} onReset={this.handleReset} />
+          <Counters counters={this.state.counters} onIncrement={this.handleIncrement} onDelete={this.handleDelete} onReset={this.handleReset} onDecrement={this.handleDecrement} />
         </main>
       </>
     );
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,35 +1,48 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  render() {
-    return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
-        >
-          Delete
-        </button>
-      </div>
-    );
-  }
-
   getBadgeClasses() {
+    const { value } = this.props.counter;
     let classes = "badge m-2 bg-";
-    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    classes += value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
     const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
+  }
+
+  render() {
+    const { counter, onIncrement, onDecrement, onDelete } = this.props;
+    return (
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            onClick={() => onIncrement(counter)}
+            className="btn btn-secondary btn-sm"
+          >
+            +
+          </button>
+          <button
+            onClick={() => onDecrement(counter)}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={counter.value === 0 ? "disabled" : ""}
+          >
+            -
+          </button>
+          <button
+            onClick={() => onDelete(counter.id)}
+            className="btn btn-danger btn-sm"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
