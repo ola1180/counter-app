437c8ae6ad5b6efe3c1c43662d91b6774f4c3d7e
Ola <aleksandra.kurdel@gmail.com>
2024-01-25 16:38:03 +0100

    code refactoring

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Number of counters:
         <span className="badge badge-pill bg-secondary ms-2">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
